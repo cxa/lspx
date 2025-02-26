1b609bc9d87687f94bf8936f5f4cde8991180752
Charles Lowell <cowboyd@frontside.com>
2025-02-26 12:27:00 -0600

    🎉initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# lspx
+
+`lspx` is a language server multiplexer, supervisor, and interactive shell.
+
+```
+Usage: lspx [options]
+
+Options:
+  -h, --help              Show help
+  -i, --interactive       start an interactive session with a multiplexed system (default: false)
+      --lsp <string ...>  start and muliplex a server with specified command string                [required]
+```
+
+## multiplexer
+
+There are often many language servers active for a given file. For example, if
+you are editing some TypeScript for the web, you might want to run some
+combination of the following:
+
+- __typescript_ (ts) resolve symbols, provide refactorings
+- _tailwind_ (css) provides completion for tailwind utility clases
+- _eslint_ (ts) highlight warnings and errors based on project linting settings
+
+In order to provide this union of functionality, IDEs like VSCode must manage
+four separate language server processes and then handle the dispatch and
+synchronization of all edits and user inputs to each one. What this means in
+practice is that in the example above, if you hover over a symbol, that hover
+should be sent to each of the typescript, tailwind, htmx, and eslint servers.
+Then any hints, overlays that any of them have should be collated and displayed
+at that point. This is a complex process, and furthermore it is required that it
+be duplicated inside every single IDE that wants to use more than one language
+server per buffer.
+
+`lspx` combines the capabilities of any number language servers into one, so
+that each IDE only needs to interact with a single LSP connection.
+
+To run the three language servers above in unison:
+
+```
+lspx --lsp "typescript-language-server --stdio" --lsp "tailwindcss-intellisense" --lsp "eslint-lsp --stdio"
+```
+
+## supervisor
+
+`lspx` manages the language server processes that it proxies and will attempt to
+restart them if they fail. How many times and at what interval is configurable.
+
+## shell
+
+`lspx` allows you to send commands by to the set of lsp, and print their
+responses
+
+```
+lspx --lsp "deno lsp" --lsp "tailwindcss-intellisense"
+
+lspx
+|
++-> deno lsp
++-> tailwindcss-intellisense
+
+LSP> initialize({ "capabilities": {} })
+```
---
+++ main.ts
@@ -0,0 +1,98 @@
+import {
+  call,
+  createSignal,
+  main,
+  type Operation,
+  resource,
+  type Stream,
+} from "effection";
+
+import * as rpc from "vscode-jsonrpc/node.js";
+import { Readable, Writable } from "node:stream";
+import { createInterface } from "node:readline";
+import * as z from "zod";
+import { parser } from "zod-opts";
+
+await main(function* (argv) {
+  let opts = parser()
+    .name("lspx")
+    .options({
+      "interactive": {
+        type: z.boolean().default(false),
+        alias: "i",
+        description: "start an interactive session with a multiplexed system",
+      },
+      "lsp": {
+        type: z.array(z.string()),
+        description: "start and muliplex a server with specified command string",
+      },
+    }).parse(argv);
+  console.log(opts);
+});
+
+export function useCommand(
+  ...params: ConstructorParameters<typeof Deno.Command>
+): Operation<Deno.ChildProcess> {
+  let [name, options] = params;
+  return resource(function* (provide) {
+    let controller = new AbortController();
+    let { signal } = controller;
+    let command = new Deno.Command(name, {
+      ...options,
+      signal,
+    });
+    let process = command.spawn();
+    try {
+      yield* provide(process);
+    } finally {
+      controller.abort();
+      yield* call(() => process.status);
+    }
+  });
+}
+
+export interface ConnectionOptions {
+  write: WritableStream<Uint8Array>;
+  read: ReadableStream<Uint8Array>;
+}
+
+export function useConnection(
+  options: ConnectionOptions,
+): Operation<rpc.MessageConnection> {
+  return resource(function* (provide) {
+    const connection = rpc.createMessageConnection(
+      //@ts-expect-error 🤷
+      new rpc.StreamMessageReader(Readable.fromWeb(options.read)),
+      new rpc.StreamMessageWriter(Writable.fromWeb(options.write)),
+    );
+    connection.listen();
+    try {
+      yield* provide(connection);
+    } finally {
+      connection.dispose();
+    }
+  });
+}
+
+export function useReadline(
+  ...args: Parameters<typeof createInterface>
+): Stream<string, never> {
+  return resource(function* (provide) {
+    let rl = createInterface(...args);
+    let lines = createSignal<string, never>();
+
+    let subscription = yield* lines;
+    try {
+      rl.on("line", lines.send);
+      yield* provide({
+        *next() {
+          rl.prompt();
+          return yield* subscription.next();
+        },
+      });
+    } finally {
+      rl.off("line", lines.send);
+      rl.close();
+    }
+  });
+}
